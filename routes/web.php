86285d965b5725b7e8e67c09ca066954ef831e22
Christopher Ortiz Ramos <l22260148@matamoros.tecnm.mx>
2026-07-25 16:40:15 -0500

    Agregadas vistas  welcome, login y AuthController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+USE App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [AuthController::class, 'loginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
