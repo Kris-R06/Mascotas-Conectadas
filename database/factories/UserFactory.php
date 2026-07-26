352672cc51f1c69361774d262d6ff9406682d9fa
Daniel Castellanos Gallardo <danielito.cg.31@gmail.com>
2026-07-25 19:09:17 -0500

    Index home funcional con seeders

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,6 +30,11 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'tipo_user_id' => 1,
+            'telefono' => fake()->numerify('##########'),
+            'direccion' => fake()->streetAddress(),
+            'has_yard' => fake()->boolean(),
+            'kids' => fake()->boolean(),
         ];
     }
 
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,8 @@ use App\Http\Controllers\ReporteController;
 use App\Http\Controllers\TipoReporteController;
 use App\Http\Controllers\TipoUserController;
 use Illuminate\Support\Facades\Route;
-USE App\Http\Controllers\AuthController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,9 +19,56 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::resource('tipo-users', TipoUserController::class)->only(['index', 'create', 'store', 'edit', 'update', 'destroy']);
-Route::resource('especies', EspecieController::class)->only(['index', 'create', 'store', 'edit', 'update', 'destroy']);
-Route::resource('tipo-reportes', TipoReporteController::class)->only(['index', 'create', 'store', 'edit', 'update', 'destroy']);
-Route::resource('mascotas', MascotaController::class)->only(['index', 'create', 'store', 'edit', 'update', 'destroy']);
-Route::resource('reportes', ReporteController::class)->only(['index', 'create', 'store', 'edit', 'update', 'destroy']);
-Route::resource('adopciones', AdopcionController::class)->only(['index', 'create', 'store', 'edit', 'update', 'destroy']);
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+    Route::get('/adopciones', [AdopcionController::class, 'index'])->name('index');
+    Route::get('/adopciones/create', [AdopcionController::class, 'create'])->name('create');
+    Route::post('/adopciones', [AdopcionController::class, 'store'])->name('store');
+    Route::get('/adopciones/edit/{adopcion}', [AdopcionController::class, 'edit'])->name('edit');
+    Route::put('/adopciones/{adopcion}', [AdopcionController::class, 'update'])->name('update');
+    Route::delete('/adopciones/destroy/{adopcion}', [AdopcionController::class, 'destroy'])->name('destroy');
+
+    // Rutas de Especies
+    Route::get('/especies', [EspecieController::class, 'index'])->name('especies.index');
+    Route::get('/especies/create', [EspecieController::class, 'create'])->name('especies.create');
+    Route::post('/especies', [EspecieController::class, 'store'])->name('especies.store');
+    Route::get('/especies/{especie}/edit', [EspecieController::class, 'edit'])->name('especies.edit');
+    Route::put('/especies/{especie}', [EspecieController::class, 'update'])->name('especies.update');
+    Route::delete('/especies/{especie}', [EspecieController::class, 'destroy'])->name('especies.destroy');
+
+    // Rutas de Mascotas
+    Route::get('/mascotas', [MascotaController::class, 'index'])->name('mascotas.index');
+    Route::get('/mascotas/create', [MascotaController::class, 'create'])->name('mascotas.create');
+    Route::post('/mascotas', [MascotaController::class, 'store'])->name('mascotas.store');
+    Route::get('/mascotas/{mascota}/edit', [MascotaController::class, 'edit'])->name('mascotas.edit');
+    Route::put('/mascotas/{mascota}', [MascotaController::class, 'update'])->name('mascotas.update');
+    Route::delete('/mascotas/{mascota}', [MascotaController::class, 'destroy'])->name('mascotas.destroy');
+
+    // Rutas de Reportes
+    Route::get('/reportes', [ReporteController::class, 'index'])->name('reportes.index');
+    Route::get('/reportes/create', [ReporteController::class, 'create'])->name('reportes.create');
+    Route::post('/reportes', [ReporteController::class, 'store'])->name('reportes.store');
+    Route::get('/reportes/{reporte}/edit', [ReporteController::class, 'edit'])->name('reportes.edit');
+    Route::put('/reportes/{reporte}', [ReporteController::class, 'update'])->name('reportes.update');
+    Route::delete('/reportes/{reporte}', [ReporteController::class, 'destroy'])->name('reportes.destroy');
+
+    // Rutas de Tipo de Reportes
+    Route::get('/tipo-reportes', [TipoReporteController::class, 'index'])->name('tipo-reportes.index');
+    Route::get('/tipo-reportes/create', [TipoReporteController::class, 'create'])->name('tipo-reportes.create');
+    Route::post('/tipo-reportes', [TipoReporteController::class, 'store'])->name('tipo-reportes.store');
+    Route::get('/tipo-reportes/{tipoReporte}/edit', [TipoReporteController::class, 'edit'])->name('tipo-reportes.edit');
+    Route::put('/tipo-reportes/{tipoReporte}', [TipoReporteController::class, 'update'])->name('tipo-reportes.update');
+    Route::delete('/tipo-reportes/{tipoReporte}', [TipoReporteController::class, 'destroy'])->name('tipo-reportes.destroy');
+
+    // Rutas de Tipo de Usuarios
+    Route::get('/tipo-users', [TipoUserController::class, 'index'])->name('tipo-users.index');
+    Route::get('/tipo-users/create', [TipoUserController::class, 'create'])->name('tipo-users.create');
+    Route::post('/tipo-users', [TipoUserController::class, 'store'])->name('tipo-users.store');
+    Route::get('/tipo-users/{tipoUser}/edit', [TipoUserController::class, 'edit'])->name('tipo-users.edit');
+    Route::put('/tipo-users/{tipoUser}', [TipoUserController::class, 'update'])->name('tipo-users.update');
+    Route::delete('/tipo-users/{tipoUser}', [TipoUserController::class, 'destroy'])->name('tipo-users.destroy');
+
+});
+
